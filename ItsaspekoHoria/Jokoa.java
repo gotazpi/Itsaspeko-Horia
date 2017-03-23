d1a68b15d91487e98813b828c7f3cea7cfa6e805
gotazpi <alain.pani@hotmail.com>
2017-03-23 11:20:16 +0100

    no message

--- ItsaspekoHoria/Jokoa.java
+++ ItsaspekoHoria/Jokoa.java
@@ -1,5 +1,32 @@
 package ItsaspekoHoria;
 
-public class Jokoa {
+import java.util.Observable;
+
+public class Jokoa{
+	
+	private static Jokoa nireJokoa;
+	private Taula erabiltzailearenTaula;
+	private Taula ordenagailuarenTaula;
+	
+	private Jokoa(){
+		super();
+	}
+	
+	public static Jokoa getJokoa(){
+		if (nireJokoa==null){
+			nireJokoa=new Jokoa();
+		}
+		return nireJokoa;
+	}
+	
+	public boolean irabazi(){
+		//TODO
+		return false;
+	}
+	
+	public boolean galdu(){
+		//TODO
+		return false;
+	}
 
 }
