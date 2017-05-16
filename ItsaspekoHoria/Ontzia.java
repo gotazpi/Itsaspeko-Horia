6473d742e863f77f64923957d7ea50ec889f3bf5
gotazpi <alain.pani@hotmail.com>
2017-05-16 21:58:00 +0200

    no message

--- ItsaspekoHoria/Ontzia.java
+++ ItsaspekoHoria/Ontzia.java
@@ -102,7 +102,7 @@ public abstract class Ontzia {
 	public boolean koodenadaGuztiakIkututa(){
 		boolean emaitza=true;
 		int i=0;
-		while(emaitza && i<=this.koordenatuak.length){
+		while(emaitza && i<=this.koordenatuak.length-1){
 			if (!this.koordenatuak[i].ikutua()){
 				emaitza=false;
 			}
